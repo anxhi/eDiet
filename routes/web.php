2cc08c3bedc00c24ac04fc9b3cad8b7e7fdb29a9
bernard <bernard.kllogjri@softlab.al>
2018-11-15 02:17:45 +0100

    minor refactor

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,5 @@
 use eDiet\Router as Route;
 
 Route::get('/','MainController@index');
-Route::get('/test',function(){
-    dd('It Works!');
-});
+Route::get('/test',function(){dd('IT works');});
+Route::get('/test/{message}',function($message){dd('Hello '.$message);});
