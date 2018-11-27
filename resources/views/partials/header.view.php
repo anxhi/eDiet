e239f1ba4dac9bcba49db9fe5a1b132964f59879
anxhi <anxhi_subashi@live.com>
2018-11-27 23:55:33 +0100

    commit message

--- resources/views/partials/header.view.php
+++ resources/views/partials/header.view.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html>
 <head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="/css/app.css">
 </head>
 <body>
@@ -21,3 +22,4 @@
     </div>
 </div>
 <div id="content">
+<div class="page">
